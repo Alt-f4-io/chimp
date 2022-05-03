489eaa4f97f8c8b19558fabb2496abca33c9c93b
Alt-f4-io <stephenaloko7@gmail.com>
2022-05-03 20:26:40 +0100

    ayo if metro dosent trust you imma shoot u

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -112,17 +112,10 @@ const Header = ({
                       navPosition && `header-nav-${navPosition}`
                     )}>
                     <li>
-                      <Link to="#0" onClick={closeMenu}>Documentation</Link>
+                      <p>Save The Chimps</p>
                     </li>
                   </ul>
-                  {!hideSignin &&
-                    <ul
-                      className="list-reset header-nav-right"
-                    >
-                      <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
-                      </li>
-                    </ul>}
+                  
                 </div>
               </nav>
             </>}
